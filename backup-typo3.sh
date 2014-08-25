8f86db98c75ce6e8656d7329b9a987688482a402
Henrik Ziegenhain <henrik@ziegenhain.me>
2014-08-25 10:48:14 +0200

    [TASK] Add GPL Headers and give credits

--- backup-typo3.sh
+++ backup-typo3.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-# TYPO3 Backup-Script
+##########################################################################
+# (c) 2014 Henrik Ziegenhain <cerdanyohann@yahoo.fr>
+# All rights reserved
+#
+# This program is free software : you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# The GNU General Public License can be found at
+# http://www.gnu.org/copyleft/gpl.html.
+#
+# This script is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# The basic backup script was written by Clemens Siebenhaar
 # Version 1.0 (23.05.2013)
-
 # Autor: Clemens Siebenhaar / 7ITec GmbH / www.7itec.de
+##########################################################################
 
 # Setzen der Instanz-Variablen aus Übergabe
 if [ "$1" = "" ]; then
